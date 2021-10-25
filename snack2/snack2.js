d7c781664091850f9baf13edf5deed4a63076f85
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-10-25 17:19:40 +0200

    punti e falli

---
+++ snack2/snack2.js
@@ -0,0 +1,30 @@
+const listaSquadre = [
+    { nome: 'Milan', punti: 0, falli: 0 },
+    { nome: 'Juventus', punti: 0, falli: 0 },
+    { nome: 'Inter', punti: 0, falli: 0 },
+    { nome: 'Roma', punti: 0, falli: 0 },
+    { nome: 'Palermo', punti: 0, falli: 0 },
+    { nome: 'Atalanta', punti: 0, falli: 0 },
+    { nome: 'Fiorentina', punti: 0, falli: 0 },
+    { nome: 'Napoli', punti: 0, falli: 0 },
+    { nome: 'Torino', punti: 0, falli: 0 },
+    { nome: 'Sassuolo', punti: 0, falli: 0 },
+];
+
+function valoriRandom(min, max) {
+    const numeroRandom = Math.floor(Math.random() * (max - min + 1) + min);
+    return numeroRandom;
+}
+
+for (i = 0; i < listaSquadre.length; i++) {
+
+    let randomPoints = valoriRandom(0, 100);
+    listaSquadre[i].punti = randomPoints;
+
+
+    let randomFouls = valoriRandom(0, 20);
+    listaSquadre[i].falli = randomFouls;
+
+}
+
+console.log(listaSquadre);
